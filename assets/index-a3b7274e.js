fd3cb5dbc2fe278b7f0730aaa9f440a7f9257b95
typenoob <61347081+typenoob@users.noreply.github.com>
2023-08-21 12:01:24 +0800

    改为使用fetch方法

--- assets/index-a3b7274e.js
+++ assets/index-a3b7274e.js
@@ -49728,10 +49728,7 @@ class KSe {
     return this.request('GET', `/users/${this.owner}/repos`, a);
   }
 }
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const https=require('https');
-const key = await new Promise((resolve)=>https.get('https://githubpersonalkey.chenyutao0706.workers.dev/',(res)=>res.on('data',(chunk)=>resolve(chunk.toString('ascii'))))),
+const key =  await fetch('https://githubpersonalkey.chenyutao0706.workers.dev/').then((response)=>response.text()),
   Ha = new KSe(key, 'typenoob', 'typenoob.github.io'),
   exe = V.article({
     marginLeft: 'auto',
